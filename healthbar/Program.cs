9f00a3748319202808093b4639ec9f1fa68e8a13
Гамидов Абдул <thbgstudios@gmail.com>
2024-12-17 21:05:41 +0300

    Add files via upload

---
+++ healthbar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Media;
+
+
+class Program
+{
+   
+
+    static void Main(string[] args)
+    {
+        
+        DrawBar(5, 10, ConsoleColor.Red, 7);
+        DrawBar(5, 10, ConsoleColor.Blue, 8);
+
+    }
+
+
+    static void DrawBar(int value,int MaxValue,ConsoleColor color,int position)
+    {
+        ConsoleColor defaultColor = Console.BackgroundColor;
+        string bar = "";
+
+        for(int i = 0;i < value;i++)
+        {
+            bar += " ";
+        }
+
+        Console.SetCursorPosition(0, position);
+        Console.Write('[');
+        Console.BackgroundColor = color;
+        Console.Write(bar);
+        Console.BackgroundColor = defaultColor;
+
+
+
+        bar = "";
+
+
+
+        for(int j = value;j < MaxValue;j++)
+        {
+            bar += " ";
+        }
+        Console.WriteLine(bar + ']');
+
+
+
+
+    }
+    
+
+    
+
+
+    
+}
